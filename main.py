f578e071cc025fac678e14a51efb6a26540e6dca
yoptohlejepeta <peta.kotlan@seznam.cz>
2024-05-03 14:47:02 +0200

    send with logo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import smtplib
 from email.message import EmailMessage
-from email.mime.image import MIMEImage
 from loguru import logger
 from jinja2 import Environment, FileSystemLoader
 
@@ -16,25 +15,22 @@ def main():
 
         env = Environment(loader=FileSystemLoader("."))
         template = env.get_template("index.html")
-
+        
+        with open('./images/logo2.svg', 'r') as file:
+            logo = file.read()
+            
         variables = {
             "to_name": "John Doe",
             "subject_info": "This is a test email",
             "body": "This is a test email body.",
             "phone": "+1234567890",
             "email": settings.smtp.user,
+            "logo": logo,
         }
 
         html = template.render(variables)
 
         msg.add_alternative(html, subtype="html")
-        
-        # with open('./images/ujep_logo.png', 'rb') as file:
-        #     img_data = file.read()
-
-        # img = MIMEImage(img_data)
-        # img.add_header('Content-ID', '<logo_cid>')
-        # msg.attach(img)
 
         with smtplib.SMTP_SSL(settings.smtp.server, settings.smtp.port) as smtp:
             smtp.login(settings.smtp.user, settings.smtp.password.get_secret_value())
